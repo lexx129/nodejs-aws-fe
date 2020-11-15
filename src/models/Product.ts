8ebad256190eb6e00ef4416fe961cac1adad7665
Aleksei Gavrilov <aleksei.gavrilov@thomsonreuters.com>
2020-11-15 22:12:55 +0400

    task-4 some fixes to make FE work with BE

--- src/models/Product.ts
+++ src/models/Product.ts
@@ -2,13 +2,14 @@ import * as Yup from 'yup';
 
 export type Product = {
   id: string,
-  title: string,
   description: string,
+  flavor: string,
   price: number,
+  count: number,
 };
 
 export const ProductSchema = Yup.object().shape({
-  title: Yup.string().required(),
   description: Yup.string(),
+  flavor: Yup.string().required(),
   price: Yup.number().required(),
 });
